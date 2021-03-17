a83cf536efe60987c98cdc0e43d0d77cd0e87ee3
Efe Evci <efeberkeevci@gmail.com>
2021-03-17 10:43:13 +0300

    Added cross origin request handler module

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { getDays, getActivities, getTags, postNewDay, postNewActivity, postNewTag, dbConn, initConnectiontoDB } = require("./db")
+const { getDays, getActivities, getTags, postNewDay, postNewActivity, postNewTag, dbConn, initConnectiontoDB } = require("./db");
+const cors = require('cors');
 
 const app = express()
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(cors());
+
 
 app.listen(process.env.PORT, () => {
     initConnectiontoDB(dbConn);
