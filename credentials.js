2d7ee7526ebbf64764dad945d3e4cb846b559e74
Efe Evci <efeberkeevci@gmail.com>
2021-08-24 18:28:40 -0700

    Revert "Merge branch 'master' of github.com:efeberkeevci/personal_website"
    This reverts commit ad904b5b505f7528d11b469028643efbd489d1ca, reversing
    changes made to df816bf2a3b8dc1c680a0642f59e6ddd642ab2be.

--- credentials.js
+++
@@ -1,8 +0,0 @@
-module.exports.credentials = {
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
-    ssl: true
-}
